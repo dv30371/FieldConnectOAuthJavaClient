fdd604ff6a737d7d9ee4ae1d0fe6f3fb4e8e905f
Dinesh Vishwakarma <VishwakarmaDinesh@johndeere.com>
2017-03-13 13:57:11 -0500

    Revert "FieldConnect OAuthJava client public git initial checkin"
    This reverts commit 715b2889355845ab57bfd476bf86dd22d66c46cb.

--- src/main/java/com/deere/democlient/ApiCredentials.java
+++
@@ -1,9 +0,0 @@
-package com.deere.democlient;
-
-import com.deere.rest.OAuthClient;
-import com.deere.rest.OAuthToken;
-
-public abstract class ApiCredentials {
-    public static final OAuthClient CLIENT = new OAuthClient("OAuth client", "OAuth secret");
-    public static final OAuthToken TOKEN = new OAuthToken("generated token", "generated secret");
-}
